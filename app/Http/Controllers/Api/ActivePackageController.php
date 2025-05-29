becb2eab02f3b663c20241c1edf1be0c32557f4d
Lucas John <lucasjohn414@gmail.com>
2025-05-29 15:28:15 +0300

    update on user active package

---
+++ app/Http/Controllers/Api/ActivePackageController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\ActivePackageRequest;
+use App\Models\ActivePackage;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\DB;
+
+class ActivePackageController extends Controller
+{
+    public function getActivePackage(ActivePackageRequest $request)
+    {
+        $activePackage = ActivePackage::with('package')
+        ->latest()
+        ->where('user_id', Auth::id())
+        ->first();
+        return response()->json(['activePackage' => $activePackage, ], 200);
+    }
+    
+    public function activatePackage(ActivePackageRequest $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'packageId' => 'required|integer',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['message' => 'Fill in all empty fields', 'errors' => $validator->errors()], 400);
+        }
+
+        $package_id = $request->input('packageId');
+
+        // Check for existing package
+        $existingPackage = ActivePackage::where('user_id', Auth::id())
+        ->where('package_id', $package_id)
+        ->first();
+
+        if ($existingPackage) {
+            return response()->json(['message' => 'Package already active.'], 409);
+        }
+
+        // Create or update ative package
+        DB::table('active_packages')->updateOrInsert(
+            ['user_id' => Auth::id()], 
+            ['package_id' => $package_id, 'user_id' => Auth::id(), 'updated_at' => now(), 'created_at' => now()]
+        );
+
+        return response()->json(['message' => 'Package has been activated.'], 200);
+    }
+}
