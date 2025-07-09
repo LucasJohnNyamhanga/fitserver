43ebf20933cda619bf76aabc382bdadc3429cc07
Lucas John <lucasjohn414@gmail.com>
2025-07-09 16:26:45 +0300

    update

--- database/migrations/2025_07_07_153853_create_payments_table.php
+++ database/migrations/2025_07_07_153853_create_payments_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('payments', function (Blueprint $table) {
             $table->id();
-            $table->foreign('package_id')->references('id')->on('packages')->onDelete('cascade');
+            $table->foreignId('package_id')->constrained()->onDelete('cascade');
             $table->string('reference')->unique();
             $table->string('status')->default('pending');
             $table->string('transaction_id')->nullable();
